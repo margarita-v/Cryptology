29e99b261aa202e8ab2366a012d5f6f043906b3f
Margarita <margo.himera@yandex.ru>
2017-10-30 12:20:59 +0300

    Small change

--- rsa.py
+++ rsa.py
@@ -1,15 +1,17 @@
 from phi import phi, phi_improved, isPrime
 from modular_inverse import modinv
 
+r, a = 66899179, 9467
+euler = int(phi_improved(r))
+
 def rsa_key(r, a):
-    return modinv(a, int(phi(r)))
+    return modinv(a, euler)
 
 def euler_brut(r, a):
-    euler = int(phi_improved(r))
     for i in range(euler, 0, -1):
         if (a * i - 1) % euler == 0:
             return i
 
-r, a = 66899179, 9467
-print(rsa_key(r, a))
-#print(euler_brut(r, a))
+if __name__ == '__main__':
+    print(rsa_key(r, a))
+    #print(euler_brut(r, a))
