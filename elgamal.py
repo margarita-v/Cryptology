90e60fac409ddfb6b6972631f38b98d4f608a07d
Margarita <margo.himera@yandex.ru>
2017-11-13 23:09:56 +0300

    El-Gamal scheme was added

---
+++ elgamal.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Для схемы шифрования Эль-Гамаля расшифровать сообщение Q2 от абонента А,
+# при условии, что абонент А не менял сессионный ключ k.
+
+from modular_inverse import modinv
+
+def get_second_message(b1, b2, p, q1):
+    return q1 * b2 * modinv(b1, p)
+
+if __name__ == '__main__':
+    p = 54751
+    b1, b2, q1 = 50973, 4246, 123
+    print(get_second_message(b1, b2, p, q1))
