571bb3bc63292d1feb3abbaba2ab8e887ee61ac8
Margarita <margo.himera@yandex.ru>
2017-11-13 23:37:57 +0300

    Refactoring for a checking abonent function

--- check_abonent.py
+++ check_abonent.py
@@ -1,7 +1,12 @@
-def check_abonent(p, q, g, y, r, s, e):
-    result = r == (g**s * y ** e) % p
-    # find secret key if result is True
-    return result
+# -*- coding: utf-8 -*-
+# Протокол аутентификации Шнорра (Schnorr)
+# Мы - абонент B. Проверить, что мы общаемся с абонентом A.
+
+from modular_inverse import modinv
+from log import log
+
+def check_abonent(r, g, y, p, s, e):
+    return r == (g**s * y ** e) % p
 
 p = 33107
 q = 16553
@@ -10,7 +15,15 @@ y = 9107
 r = 32607
 
 if __name__ == '__main__':
-    print(check_abonent(p, q, g, y, r, 9856, 15776))
-    print(check_abonent(p, q, g, y, r, 8108, 490))
-    print(check_abonent(p, q, g, y, r, 7309, 9987))
-    print(check_abonent(p, q, g, y, r, 1267, 155))
+    print(check_abonent(r, g, y, p, 9856, 15776))
+    print(check_abonent(r, g, y, p, 8108, 490))
+    print(check_abonent(r, g, y, p, 7309, 9987))
+    print(check_abonent(r, g, y, p, 1267, 155))
+
+    # find secret key
+    inverse_elem = modinv(g, p)
+    print(inverse_elem)
+    # find a
+    a = log(g, r, p)
+    print(a)
+    #print( (s - a) * modinv(e, p))
